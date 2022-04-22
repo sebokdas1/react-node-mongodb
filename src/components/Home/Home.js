e7c1380dc819831e92c4da5487880182fb3d0de2
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-22 18:07:00 +0600

    all setup completed

---
+++ src/components/Home/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, []);
+
+    const handleUserDelete = id => {
+        const proceed = window.confirm('are you sure you want to delete?')
+        if (proceed) {
+            console.log('deleting user with id:', id);
+            const url = `http://localhost:5000/user/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        console.log(data)
+                        const remaining = users.filter(user => user._id !== id);
+                        setUsers(remaining);
+                    }
+                })
+        }
+    }
+    return (
+        <div>
+            <h3>available user: {users.length}</h3>
+            <ul>
+                {
+                    users.map(user => <li
+                        key={user._id}
+                    >
+                        {user.name}: {user.email}
+                        <Link to={`/update/${user._id}`}><button>Update</button></Link>
+                        <button onClick={() => handleUserDelete(user._id)}>x</button>
+                    </li>)
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default Home;
